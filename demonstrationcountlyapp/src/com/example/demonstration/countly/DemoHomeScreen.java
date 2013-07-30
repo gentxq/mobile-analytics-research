1c3d07ce1604105b16a0d5055142403c1b3d9f24
Julian Harty <julianharty@gmail.com>
2013-07-30 20:48:40 +0100

    Created a new bare Android project to be a demonstration app for SQAPI. The app will be developed as I continue my research and experimentation.

---
+++ demonstrationcountlyapp/src/com/example/demonstration/countly/DemoHomeScreen.java
@@ -0,0 +1,15 @@
+package com.example.demonstration.countly;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DemoHomeScreen extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
